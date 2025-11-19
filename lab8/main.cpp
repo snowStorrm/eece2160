15b3a0e79099eac886c60ed683bcdac349c268f1
snowStorrm <ryanjz101@gmail.com>
2025-11-19 16:27:35 -0500

    commenting

--- lab8/main.cpp
+++ lab8/main.cpp
@@ -3,13 +3,19 @@
 using namespace std;
 
 int main() {
+    // instantiate new DE1_SOC instance
     DE1SOC* DE1 = new DE1SOC();
+    // loop infinitely
     while (true) {
         int a;
+        // get user input
         cout << "Please input an angle to set the servo to (0 - 180). Enter -1 to quit: ";
         cin >> a;
+        // if input is -1, quit program
         if (a == -1) break;
+        // clamping function between 0 and 180 degrees
         a = (a < 0) ? 0 : (a > 180) ? 180 : a;
+        // set the register holding the angle value
         DE1->registerWrite(PIO_BASE, a);
     }
 }
